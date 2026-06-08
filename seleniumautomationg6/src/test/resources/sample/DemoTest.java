028128ddb794a9f0483a775e1588e4850bbb0ade
Aditya Singh <Aditya Singh@Y4_Max>
2026-06-09 00:28:08 +0530

    i delete something

--- seleniumautomationg6/src/test/resources/sample/DemoTest.java
+++ seleniumautomationg6/src/test/resources/sample/DemoTest.java
@@ -3,10 +3,6 @@ package sample;
 public class DemoTest {
 	
 	public static void main(String[] args) {
-		public void add() {
-			System.out.println("this is main method");
-			System.out.println("hii");
-		}
-	}
+	
 	
 }
